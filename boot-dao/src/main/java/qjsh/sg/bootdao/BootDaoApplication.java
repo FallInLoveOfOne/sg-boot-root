cdbb49b10fb819b7358b3a3b16acf9cf34f31f06
susu <lk19931990@163.com>
2019-11-16 19:07:24 +0800

    添加多模块

---
+++ boot-dao/src/main/java/qjsh/sg/bootdao/BootDaoApplication.java
@@ -0,0 +1,13 @@
+package qjsh.sg.bootdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDaoApplication.class, args);
+    }
+
+}
---
+++ boot-dao/src/test/java/qjsh/sg/bootdao/BootDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package qjsh.sg.bootdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
