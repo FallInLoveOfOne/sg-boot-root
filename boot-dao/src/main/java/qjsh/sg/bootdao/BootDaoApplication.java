3ba01bb7a6876ec78e55bbccc52dca03cadc959b
susu <lk19931990@163.com>
2019-11-16 19:15:32 +0800

    修改

--- boot-dao/src/main/java/qjsh/sg/bootdao/BootDaoApplication.java
+++ boot-dao/src/main/java/qjsh/sg/bootdao/BootDaoApplication.java
@@ -2,8 +2,9 @@ package qjsh.sg.bootdao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BootDaoApplication {
 
     public static void main(String[] args) {
